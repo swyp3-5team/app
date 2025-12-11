2de3ed27997687299eb80c849e28285421a10fb7
Lee Inho <inhoking01@gmail.com>
2025-12-11 22:53:50 +0900

    feat. Kakao SDK 연동

--- Cloverly/Application/AppDelegate.swift
+++ Cloverly/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let kakaoNativeAppKey = Bundle.main.infoDictionary?["NATIVE_APP_KEY"] as? String ?? ""
+        KakaoSDK.initSDK(appKey: kakaoNativeAppKey)
         return true
     }
 
