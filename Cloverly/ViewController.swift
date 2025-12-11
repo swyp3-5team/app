10ecdd10bffedb073136c65b3b22a65edeb8b055
Lee Inho <inhoking01@gmail.com>
2025-12-11 22:28:28 +0900

    init project

---
+++ Cloverly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cloverly
+//
+//  Created by 이인호 on 12/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CloverlyUITests/CloverlyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CloverlyUITestsLaunchTests.swift
+//  CloverlyUITests
+//
+//  Created by 이인호 on 12/11/25.
+//
+
+import XCTest
+
+final class CloverlyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
