9f8b57243df9371748c627edac6314b66270a6f0
Lee Inho <inhoking01@gmail.com>
2025-12-25 22:32:36 +0900

    chore. 목업 메세지 제거

--- Cloverly/Domain/Entities/Message.swift
+++ Cloverly/Domain/Entities/Message.swift
@@ -21,24 +21,3 @@ struct Message {
     let kind: MessageKind
     let chatType: ChatType
 }
-
-class Mock {
-    static func getMockMessages() -> [Message] {
-        let messages = ["Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say",
-                        "that they were perfectly normal, thank you very much. They were the last",
-                        "people you'd expect to be involved in anything strange or mysterious",
-                        "because they just didn't hold with such nonsense.",
-        "Mr. Dursley was the director of a firm called Grunnings",
-                        "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say",
-                                        "that they were perfectly normal, thank you very much. They were the last",
-                                        "people you'd expect to be involved in anything strange or mysterious",
-                                        "because they just didn't hold with such nonsense.",
-                        "Mr. Dursley was the director of a firm called Grunnings",
-                        "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say",
-                                        "that they were perfectly normal, thank you very much. They were the last",
-                                        "people you'd expect to be involved in anything strange or mysterious",
-                                        "because they just didn't hold with such nonsense.",
-                        "Mr. Dursley was the director of a firm called Grunnings"]
-        return messages.map { Message(kind: .text($0), chatType: ChatType.allCases.randomElement()!) }
-    }
-}
