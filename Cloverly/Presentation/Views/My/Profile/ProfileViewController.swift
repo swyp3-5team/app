f5dfd1e2874976186dbcb1dcd8909872a3b8c744
Lee Inho <inhoking01@gmail.com>
2025-12-30 01:13:40 +0900

    feat. 로그아웃 및 회원탈퇴 연동

--- Cloverly/Presentation/Views/My/Profile/ProfileViewController.swift
+++ Cloverly/Presentation/Views/My/Profile/ProfileViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import RxRelay
 
 enum ProfileMenu: String, CaseIterable {
     case profile = "프로필"
@@ -64,10 +65,10 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         case 0:
             let vc = ProfileEditViewController()
             navigationController?.pushViewController(vc, animated: true)
-//        case 2:
-//            // 로그아웃
-//        case 3:
-//            // 회원탈퇴
+        case 2:
+            showLogoutAlert()
+        case 3:
+            showWithdrawAlert()
         default:
             break
         }
@@ -75,5 +76,69 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
+    }
+    
+    private func showLogoutAlert() {
+        let alert = UIAlertController(
+            title: "로그아웃하시겠어요?",
+            message: nil,
+            preferredStyle: .alert
+        )
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        let logoutAction = UIAlertAction(title: "로그아웃", style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            AuthViewModel.shared.logout()
+            
+            self.reloadRootView()
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(logoutAction)
+        
+        self.present(alert, animated: true)
+    }
+
+    private func showWithdrawAlert() {
+        let alert = UIAlertController(
+            title: "서비스를 탈퇴하시겠어요?",
+            message: "탈퇴 시 데이터가 모두 삭제되며 복구가 불가능해요.",
+            preferredStyle: .alert
+        )
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        let withdrawAction = UIAlertAction(title: "탈퇴할래요", style: .destructive) { [weak self] _ in
+            guard let self = self else { return }
+            
+            Task {
+                do {
+                    if AuthViewModel.shared.currentUser.value?.provider == .apple {
+                        try await AuthViewModel.shared.deleteAppleUser()
+                    } else {
+                        try await AuthViewModel.shared.deleteKakaoUser()
+                    }
+                    
+                    self.reloadRootView()
+                } catch {
+                    print("탈퇴 실패: \(error)")
+                }
+            }
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(withdrawAction)
+        
+        self.present(alert, animated: true)
+    }
+    
+    private func reloadRootView() {
+        DispatchQueue.main.async {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let sceneDelegate = windowScene.delegate as? SceneDelegate {
+                sceneDelegate.checkAndUpdateRootViewController()
+            }
+        }
     }
 }
