37661a42509d0ca70a68fb9fa3ba9508088094f3
Lee Inho <inhoking01@gmail.com>
2026-01-23 01:15:40 +0900

    refactor. 배경 및 캐릭터 이미지 애니메이션(webp)파일로 변경

--- Cloverly/Application/AppDelegate.swift
+++ Cloverly/Application/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import KakaoSDKCommon
 import FirebaseCore
+import SDWebImage
+import SDWebImageWebPCoder
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        SDImageCodersManager.shared.addCoder(SDImageWebPCoder.shared)
         
         let kakaoNativeAppKey = Bundle.main.infoDictionary?["NATIVE_APP_KEY"] as? String ?? ""
         KakaoSDK.initSDK(appKey: kakaoNativeAppKey)
