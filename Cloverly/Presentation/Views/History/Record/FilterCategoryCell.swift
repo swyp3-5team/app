ad779aef15c8b955af48d26aaec27928a0b80e41
Inho Lee <harrylee0116@gmail.com>
2026-06-15 14:11:49 +0900

    refactor. 카테고리셀 텍스트 크기 기존으로 원복

--- Cloverly/Presentation/Views/History/Record/FilterCategoryCell.swift
+++ Cloverly/Presentation/Views/History/Record/FilterCategoryCell.swift
@@ -14,7 +14,7 @@ class FilterCategoryCell: UICollectionViewCell {
     private let label: AppLabel = {
         let label = AppLabel()
         label.textColor = .gray1
-        label.typography = .b4
+        label.typography = .b7
         label.textAlignment = .center
         return label
     }()
@@ -60,10 +60,10 @@ class FilterCategoryCell: UICollectionViewCell {
     private func updateAppearance() {
         if isSelected {
             contentView.layer.borderColor = UIColor.green5.cgColor
-            label.typography = .b1
+            label.typography = .b5
         } else {
             contentView.layer.borderColor = UIColor.gray8.cgColor
-            label.typography = .b3
+            label.typography = .b7
         }
     }
 }
