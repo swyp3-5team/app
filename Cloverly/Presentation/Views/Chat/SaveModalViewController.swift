18fe049f5c6597326df29887bc56f03d2b7be174
Lee Inho <inhoking01@gmail.com>
2026-07-05 20:05:46 +0900

    chore. 문구 변경

--- Cloverly/Presentation/Views/Chat/SaveModalViewController.swift
+++ Cloverly/Presentation/Views/Chat/SaveModalViewController.swift
@@ -25,7 +25,7 @@ class SaveModalViewController: UIViewController {
     
     private let subtitleLabel: AppLabel = {
         let label = AppLabel()
-        label.text = "*홈 > 내역탭에서 수정이 가능합니다"
+        label.text = "*하단 [내역]탭에서 수정이 가능합니다."
         label.textColor = .gray5
         label.typography = .l1
         return label
